f3b74119b5531d7c89b6f954c053ad22c889faec
slim1979 <chinezan@yandex.ru>
2022-11-14 20:04:38 +0300

    учитывать изменение атрибутов

--- workshop_1.rb
+++ workshop_1.rb
@@ -2,9 +2,9 @@
 
 module Memoize
   def memoize(name, as: nil)
-    unbound   = instance_method(name)
-    cache_key = as.presence || "@#{name}"
+    unbound = instance_method(name)
     define_method name do |*args|
+      cache_key = "@#{as.presence || name}_#{args.hash}"
       return instance_variable_get(cache_key) if instance_variable_defined?(cache_key)
 
       cached_value = unbound.bind(self).call(*args)
