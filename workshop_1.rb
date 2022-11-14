bb09634e0718000ed83623632eb86a03dcb5c9ea
slim1979 <chinezan@yandex.ru>
2022-11-14 11:13:38 +0300

    done

---
+++ workshop_1.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Memoize
+  def memoize(name, as: nil)
+    unbound   = instance_method(name)
+    cache_key = as.presence || "@#{name}"
+    define_method name do |*args|
+      return instance_variable_get(cache_key) if instance_variable_defined?(cache_key)
+
+      cached_value = unbound.bind(self).call(*args)
+      instance_variable_set(cache_key, cached_value)
+    end
+  end
+end
+
+class Example
+  extend Memoize
+
+  memoize def sum(x, y)
+    x + y
+  end
+  memoize def foo
+    rand
+  end
+  memoize def bar
+            rand
+  end, as: :@bar_cached
+end
